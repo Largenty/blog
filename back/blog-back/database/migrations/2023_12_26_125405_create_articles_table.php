3e11b5e4735625a63ecedbee20d308eea48c1d29
largenty <largenty@gmail.com>
2023-12-26 14:02:10 +0100

    :memo: Adding column into Article

--- back/blog-back/database/migrations/2023_12_26_125405_create_articles_table.php
+++ back/blog-back/database/migrations/2023_12_26_125405_create_articles_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title', 100);
+            $table->text('description');
         });
     }
 
